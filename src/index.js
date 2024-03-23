6dfae9f57f3003d546da7ebf2adb46e86c12b00d
Jaya Parihar <jayaparihar26015@gmail.com>
2024-03-23 15:48:18 +0530

    updated env variables

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ require("./models/db");
 const app = express();
 app.use(express.json());
 
-app.use('/admin', require("./routers/restaurant-router"));
-// app.use('/category', require("./routers/category-router"))
-// app.use('/item', require("./routers/item-router"))
+const { PORT } = process.env
 
-app.listen(3000, () => {
+app.use('/admin', require("./routers/restaurant-router"));
+
+app.listen(PORT, () => {
   console.log("server started");
 });
--- src/models/db.js
+++ src/models/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const { restaurantSchema, itemSchema, categorySchema } = require('./schema')
 
-mongoose.connect(process.env.connection_string);
+const { DB_CONNECTION_STRING } = process.env
+
+mongoose.connect(DB_CONNECTION_STRING);
 
 exports.restaurantModel = mongoose.model("restaurants", mongoose.Schema(restaurantSchema));
 exports.categoryModel = mongoose.model("categories", mongoose.Schema(categorySchema));
